2390ffcaf6ed9ac8bc6d10a769f653a7ec9526b4
iammahbubalam <mdmahbubnirjon@gmail.com>
2024-03-24 05:29:27 +0600

    init

---
+++ src/main/java/com/mahbubalam/springcompilerservice/SpringCompilerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mahbubalam.springcompilerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCompilerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCompilerServiceApplication.class, args);
+	}
+
+}
