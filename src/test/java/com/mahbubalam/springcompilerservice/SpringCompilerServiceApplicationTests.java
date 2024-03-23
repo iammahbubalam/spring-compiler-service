96310b18d0c198aa02e1c88ec863b60099f71120
iammahbubalam <mdmahbubnirjon@gmail.com>
2024-03-24 05:32:28 +0600

    init

---
+++ src/test/java/com/mahbubalam/springcompilerservice/SpringCompilerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahbubalam.springcompilerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCompilerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
